53e681e036fec5b3ea8dc8b07a1ec28f4017983f
JonHalloran <angmar139@hotmail.com>
2018-04-06 10:23:18 -0700

    Make routes using google api

--- frontend/util/running_route_util.js
+++ frontend/util/running_route_util.js
@@ -1,20 +1,54 @@
 // copied from google maps api
 let map
+
+let locations = []
+let directionsService
+let directionsDisplay
+
 export const initMap = () => {
-    var uluru = {lat: 37.77949, lng: -122.4194};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    let sanFran = {lat: 37.77949, lng: -122.4194};
+    let map = new google.maps.Map(document.getElementById('map'), {
             zoom: 14,
-            center: uluru,
+            center: sanFran,
             disableDefaultUI: true,
         })
     ;
+    directionsDisplay.setMap(map)
+    map.addListener('click', (e) => {
+        placeMarker(e.latLng, map)
+    })
 
-    map.addListener('click', (e) => placeMarkerAndPanTo(e.latLng, map))
 }
 
 function placeMarker(latLng, map) {
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: latLng,
         map: map
+    });
+
+    addWaypoint(latLng)
+    calcRoute()
+}
+
+function addWaypoint(latLng) {
+    locations.push({location: latLng, stopover: true})
+    console.log(locations)
+}
+
+
+function calcRoute() {
+    let selectedMode = 'WALKING'
+    let request = {
+        origin: locations[0].location,
+        destination: locations[locations.length - 1].location,
+        waypoints: locations.slice(1, -1),
+        travelMode: google.maps.TravelMode[selectedMode]
+    }
+    directionsService.route(request, function (response, status) {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(response);
+        }
     });
 }
