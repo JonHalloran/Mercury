3c5c9a3bc3a6812d7c3a04bb95c154f966225523
JonHalloran <angmar139@hotmail.com>
2018-04-07 22:00:59 -0700

    Work on css for routes page

--- frontend/components/route/route_show.jsx
+++ frontend/components/route/route_show.jsx
@@ -3,7 +3,24 @@ import {calcRoute, initMap, mapExists} from "../../util/google_api_util";
 
 
 class RouteShow extends React.Component {
+    componentW
+
     componentDidMount() {
+        this.props.retrieveRoute(this.props.match.params.routeId).then(() => {
+            if (mapExists()) {
+                window.initMap()
+                return undefined;
+            }
+            window.initMap = initMap();
+            const script = document.createElement("script");
+            script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAdfqHssdl3Lpo_Lul6UOOGLwnfO85bbJ0&callback=initMap";
+            script.async = true;
+            document.body.appendChild(script);
+        })
+
+    }
+
+    componentWillReceivProps() {
         this.props.retrieveRoute(this.props.match.params.routeId)
         if (mapExists()) {
             window.initMap()
@@ -33,27 +50,29 @@ class RouteShow extends React.Component {
         return (
             <div className={'route-show'}>
                 <main className={'route-show-main'}>
-                    <h1 className={"route-show"}>{route.name}</h1>
+                    <div className={"route-show"}>
+                        <h1>{route.name}</h1>
+                    </div>
                     <div className={'route-show-not-title'}>
                         <div className={'distance-info'}>
                             <h4 className={'distance-label'}>DISTANCE</h4>
                             <h2 className={'distance-amount'}>{distance[0]}</h2>
-                            <h4 className={'distance-lave'}>miles</h4>
+                            <h4 className={'distance-label'}>miles</h4>
                         </div>
-                    </div>
-                    <div className={'route-details'}>
-                        <div className={'route-details-beginning'}>
-                            <p className={'route-show-title'}>BEGINS IN: </p>
-                            <p className={'route-show-information'}>{origin}</p>
-                        </div>
-                        <div className={'route-details-creator'}>
-                            <p className={'route-show-title'}>CREATED BY: </p>
-                            <p className={'route-show-information'}>{creator_name}</p>
-                        </div>
-                        <div className={'route-details-description'}>
-                            <p className={'route-show-title'}>DESCRIPTION: </p>
-                            <p className={'route-show-information'}>{route.description}</p>
-                        </div>
+                        <ul className={'route-details'}>
+                            <li className={'route-details-beginning'}>
+                                <p className={'route-show-title'}>BEGINS IN: </p>
+                                <p className={'route-show-information'}>{origin}</p>
+                            </li>
+                            <li className={'route-details-creator'}>
+                                <p className={'route-show-title'}>CREATED BY: </p>
+                                <p className={'route-show-information'}>{creator_name}</p>
+                            </li>
+                            <li className={'route-details-description'}>
+                                <p className={'route-show-title'}>DESCRIPTION: </p>
+                                <p className={'route-show-information'}>{route.description}</p>
+                            </li>
+                        </ul>
                     </div>
                     <div id={'map'}>OHHH SHITE MAP IS BROKEN</div>
                 </main>
