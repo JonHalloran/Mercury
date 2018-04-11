72691f8cb03cc6ba7037f0bb530d04d02c5e6d91
JonHalloran <angmar139@hotmail.com>
2018-04-11 10:51:33 -0700

    Basic run show stylying

--- frontend/components/run/run_show.jsx
+++ frontend/components/run/run_show.jsx
@@ -28,18 +28,37 @@ class RunShow extends React.Component {
     }
 
   }
+  convert(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor(seconds % 3600 / 60);
+    var seconds = Math.floor(seconds % 3600 % 60);
+    var time = '';
+    if (hours > 0) {
+      time += hours + ':'
+    }
+    if (minutes < 10 && hours > 0){
+      time += '0'
+    }
+    time += minutes + ':'
+    if (seconds < 10){
+      time += '0'
+    }
+    time += seconds
+    return time;
+  }
 
   render(){
 
-    let dist = 1;
-    let duration = 60;
-    let name = ';alksfjd'
+    let dist = 0;
+    let duration = 0;
+    let name = '';
     if (this.state.run && this.state.route) {
       dist = this.state.route.distance;
+      name = this.state.run.name;
       duration = this.state.run.duration;
-      name = this.state.run.name
     }
-    const pace = (duration/60)/dist
+    const time = this.convert(duration)
+    const pace = this.convert(Math.round(duration/dist))
 
 
     console.log(this.state);
@@ -51,15 +70,21 @@ class RunShow extends React.Component {
           <div className={'calculated-values'}>
             <div className={'run-show-page-value-pair'}>
               <section className={'run-show-page-value-title'} >Distance</section>
-              <section className={'run-show-page-value-number'}>{dist}</section>
+              <section className={'run-show-page-value-number'}>{dist}
+              <span className={'run-show-page-value-label'}>miles</span>
+              </section>
             </div>
             <div className={'run-show-page-value-pair'}>
               <section className={'run-show-page-value-title'} >duration</section>
-              <section className={'run-show-page-value-number'}>{duration}</section>
+              <section className={'run-show-page-value-number'}>{time}
+              <span className={'run-show-page-value-label'}></span>
+              </section>
             </div>
             <div className={'run-show-page-value-pair'}>
               <section className={'run-show-page-value-title'} >AVG Pace</section>
-              <section className={'run-show-page-value-number'}>{pace}</section>
+              <section className={'run-show-page-value-number'}>{pace}
+              <span className={'run-show-page-value-label'}>/mile</span>
+              </section>
             </div>
           </div>
           </div>
