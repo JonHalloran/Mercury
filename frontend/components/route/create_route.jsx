0bc4ddc425d1d9b7407a8bbd5bc0e3ec0c4e6b45
JonHalloran <angmar139@hotmail.com>
2018-04-10 21:07:15 -0700

    Add errors to create routes

--- frontend/components/route/create_route.jsx
+++ frontend/components/route/create_route.jsx
@@ -17,7 +17,8 @@ class CreateRoute extends React.Component {
         this.state = {
             zipcode: '',
             showRouteDetails: true,
-            routeName: ''
+            routeName: '',
+            routeErrors: []
         }
 
         this.handlChangeZipcode = this.handlChangeZipcode.bind(this);
@@ -50,38 +51,62 @@ class CreateRoute extends React.Component {
     }
 
     handleNewRoute() {
-        let response_object = getResponse();
-        let response_0 = response_object.response.routes[0];
-        let distance = response_0.legs[0].distance.text;
-        let start_location = response_0.legs[0].start_address;
-        let polyline = response_0.overview_polyline;
-        let split_origin = start_location.split(', ');
-        let origin = split_origin[1] + ", " + split_origin[2].slice(0, 2);
-        let
-            newRoute = {
-                request: JSON.stringify(response_object.response.request),
-                name: this.state.routeName,
-                description: `This is a ${distance} route that starts at ${start_location}`,
-                lat: response_object.waypointLat,
-                log: response_object.waypointLng,
-                origin: origin,
-                img_url: polyline,
-                distance: distance
-            };
-        removeAllWaypoint();
-        this.props.createRoute(newRoute).then(response => this.props.history.push(`/routes/${response.payload.route.id}`));
+      if (this.state.routeName == '') {
+        console.log("HERE!!!!");
+        this.setState({routeErrors: ['The route must have a name']})
+        return null;
+      }
+      let response_object
+      try {
+        response_object = getResponse()
+      } catch (TypeError) {
+        this.setState({routeErrors: ['The route must have 2 or more points']})
+        return null;
+      }
+      let response_0 = response_object.response.routes[0];
+      let distance = response_0.legs[0].distance.text;
+      let start_location = response_0.legs[0].start_address;
+      let polyline = response_0.overview_polyline;
+      let split_origin = start_location.split(', ');
+      let origin = split_origin[1] + ", " + split_origin[2].slice(0, 2);
+      let
+        newRoute = {
+            request: JSON.stringify(response_object.response.request),
+            name: this.state.routeName,
+            description: `This is a ${distance} route that starts at ${start_location}`,
+            lat: response_object.waypointLat,
+            log: response_object.waypointLng,
+            origin: origin,
+            img_url: polyline,
+            distance: distance
+        };
+
+      removeAllWaypoint();
+      this.props.createRoute(newRoute).then(response => this.props.history.push(`/routes/${response.payload.route.id}`), );
     }
 
 
     render() {
         let rootDetails;
-
+        let errors;
         rootDetails = this.state.showRouteDetails ?
             <form className={'route-details-form'} onSubmit={() => this.handleNewRoute()}>
                 <input className={'route-name'} placeholder={'Name this map'} value={this.state.routeName}
                        onChange={(e) => this.handleChangeName(e)}/>
                 <button className={'route-name-button'}>SAVE ROUTE</button>
             </form> : '';
+
+        errors = this.props.routeErrors ?
+        <ul className={'route-errors'} >
+          {this.state.routeErrors.map(error => <li key={error} >{error}</li>)}
+        </ul>
+        : '';
+        errors = this.state.routeErrors.length > 0 ?
+        <ul className={'route-errors'} >
+          {this.state.routeErrors.map(error => <li key={error} >{error}</li>)}
+        </ul>
+        : '';
+
         return (
             <div className={'create-route-page'}>
                 <aside className={'create-route-sidebar'}>
@@ -99,6 +124,7 @@ class CreateRoute extends React.Component {
                         <button className={'clear-map'} onClick={() => removeAllWaypoint()}>&#10006; Clear</button>
                     </div>
                     <div className={'route-details-root'}><span onClick={() => this.handleToggleRouteDetails()}>Route Details</span>
+                        {errors}
                         {rootDetails}
                     </div>
                 </aside>
--- frontend/components/route/create_route_container.js
+++ frontend/components/route/create_route_container.js
@@ -1,14 +1,15 @@
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router-dom'
-import {createRoute} from "../../actions/route_actions";
-import CreateRoute from './create_route'
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { createRoute } from "../../actions/route_actions";
+import CreateRoute from "./create_route";
 
 const mapStateToProps = state => ({
-    userId: state.session.user.userId
-})
+  userId: state.session.user.userId,
+  routeErrors: state.errors.route
+});
 
 const mapDispatchToProps = dispatch => ({
-    createRoute: (route) => dispatch(createRoute(route))
-})
+  createRoute: route => dispatch(createRoute(route))
+});
 
-export default withRouter(connect(null, mapDispatchToProps)(CreateRoute))
+export default withRouter(connect(null, mapDispatchToProps)(CreateRoute));
